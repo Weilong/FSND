9b822a8e4b24946e001edf57d8a2d91a9b4b5e2f
Weilong Ding <tonyaziten@weilongdingsmbp.home>
2016-10-13 09:24:54 +1100

    first commit

---
+++ break.py
@@ -0,0 +1,8 @@
+import time
+import webbrowser
+
+print("This program started on "+time.ctime())
+
+for i in range(3):
+	time.sleep(10)
+	webbrowser.open("https://www.youtube.com/watch?v=ITkRBB8Fdvs")
